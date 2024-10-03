ad14a736a79e80a3b593f7104d06220f65a3dc57
Sarah Armstrong <ghlasphyre@gmail.com>
2024-10-02 23:28:01 -0700

    finally got a way to get the full names

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# consider imbalanced classes, use duplicates or not in training
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-if __name__ == "__main__":
+# no point matching planet type to duplicate exoplanet names
+def getUniqueNames():
     df = pd.read_csv('exoplanet_names.csv')
     unique_names = df['pl_name'].unique()
     lst = unique_names.tolist()
@@ -10,3 +12,20 @@ if __name__ == "__main__":
         for name in lst:
             f.write(name)
             f.write('\n')
+
+if __name__ == "__main__":
+    getUniqueNames()
+    # imbalanced classes, use duplicate entries?
+    # add short name to url, then repeat to get planet types
+    url = 'https://example.com'
+    headers = {
+    'User-Agent': 'ScraperToGetExoplanetFullNamesBecauseExoplanetArchiveOnlyHasShortNames (ghlasphyre@gmail.com)'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        html_content = response.text
+    else:
+        print(f'Error: {response.status_code}')
+
+    soup = BeautifulSoup(html_content, 'html.parser')
